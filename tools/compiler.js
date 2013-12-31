cbcb82a797d79c790642eca8062710ba55da84c2
haydn paterson <haydn.developer@gmail.com>
2014-01-01 08:49:02 +1300

    updates

--- tools/compiler.js
+++ tools/compiler.js
@@ -13,17 +13,17 @@ exports.build = function(sources, arguments , output_filename , callback) {
 
     for(var n in arguments) {
 
-        sources.push(arguments[n]);
+        sources.push(arguments[n])
     }
 	
-	var tsc = require("child_process").spawn('tsc', sources, { stdio: 'inherit' });
+	var tsc = require("child_process").spawn('tsc', sources, { stdio: 'inherit' })
 	
 	tsc.on('close', function() {
 		
 	    setTimeout(function() {
             
-            callback();
+            callback()
 
-	    }, 250);
+	    }, 250)
 	});	
 }
